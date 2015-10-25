06f19ee99251b1db36a78b2b384a511475083fb1
IskraNikolova <air_iskra@yahoo.com>
2015-10-25 18:26:20 +0200

    add a new project

---
+++ Homework4JavaCollectionsBasics/src/Problem9CombineListsOfLetters.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem9CombineListsOfLetters {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstString = scanner.nextLine();
+        String[] firstArrays = firstString.split(" ");
+        ArrayList<String> list =  new ArrayList<>(Arrays.asList(firstArrays));
+
+        String secondString = scanner.nextLine();
+        String[] secondArrays = secondString.split(" ");
+        boolean isEqual = false;
+        for (int i = 0; i < secondArrays.length; i++) {
+            for (int j = 0; j < firstArrays.length; j++) {
+                if(secondArrays[i].equals(firstArrays[j])) {
+                    isEqual = true;
+                }
+            }
+            if(isEqual == false){
+                list.add(secondArrays[i]);
+            }
+            isEqual = false;
+        }
+        for (String symbol : list) {
+            System.out.print(symbol + " ");
+        }
+    }
+}
