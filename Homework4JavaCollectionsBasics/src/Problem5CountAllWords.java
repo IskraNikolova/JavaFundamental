507443cf32b44c3c3aa0c816ac4257e5608c26d5
IskraNikolova <air_iskra@yahoo.com>
2015-10-25 18:24:05 +0200

    Add a new project

---
+++ Homework4JavaCollectionsBasics/src/Problem5CountAllWords.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/*
+Write a program to count the number of words in given sentence.
+Use any non-letter character as word separator.
+ */
+
+public class Problem5CountAllWords {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        input = input.trim();
+        String[] arrayStr = input.split("[\\W]+");
+        System.out.println(arrayStr.length);
+    }
+}
