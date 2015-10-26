ec6e3fadc03b9f460580d41cf074dd2f68f685c6
IskraNikolova <air_iskra@yahoo.com>
2015-10-26 13:49:34 +0200

    add a new project

---
+++ Homework4JavaCollectionsBasics/src/Problem12CardsFrequencies.java
@@ -0,0 +1,28 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Problem12CardsFrequencies {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().trim().split(" ");
+        int numOfAllCards = input.length;
+
+
+        Map<String, Integer> linkMap = new LinkedHashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            String cardFace = input[i].substring(0, input[i].length() - 1);
+            if(!linkMap.containsKey(cardFace)) {
+                linkMap.put(cardFace, 0);
+            }
+
+            linkMap.put(cardFace, linkMap.get(cardFace) + 1);
+        }
+
+        for (Map.Entry<String, Integer> card : linkMap.entrySet()) {
+            double percentage = ((double)card.getValue() / numOfAllCards) * 100;
+            System.out.printf("%s -> %.2f%%\n", card.getKey(), percentage);
+        }
+    }
+}
