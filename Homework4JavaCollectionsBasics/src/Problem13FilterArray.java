8c8b4e459811e44eec38572f9cd829413e1d287e
IskraNikolova <air_iskra@yahoo.com>
2015-10-26 14:10:06 +0200

    Add a new project

---
+++ Homework4JavaCollectionsBasics/src/Problem13FilterArray.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Write a program that filters an array of strings,
+leaving only the strings with length greater than 3 characters. Use .filter()
+ */
+
+public class Problem13FilterArray {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toLowerCase();
+        String[] words = input.split("[\\W]+");
+
+        ArrayList<String> listWord = new ArrayList<>(Arrays.asList(words));
+        listWord.stream().filter(a -> a.length() > 3).forEach(g -> System.out.printf(g + " "));
+    }
+}
